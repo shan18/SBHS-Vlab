20d397968ce9c1accc3ca48cfda9ec9184051591
meshde <meshde.md@gmail.com>
2017-03-12 23:41:51 +0530

    Minor Changes in sbhs_formula

--- sbhs_formula/formula.py
+++ sbhs_formula/formula.py
@@ -4,13 +4,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 class Formula:
     def __init__(self):
-        self.c = 0
         x = os.path.join(BASE_DIR,'coeff.txt')
         with open(x,'r') as f:
             lines = f.readlines()
-            i = random.randint(0,len(lines)-1)
-            print lines[i]
-            self.k,self.z = lines[i].split()
-            self.k,self.z = float(self.k),float(self.z)
+            self.i = random.randint(0,len(lines)-1)
+            self.k,self.z,self.c = lines[self.i].split()
+            self.k,self.z,self.c = float(self.k),float(self.z),float(self.c)
     def getTemp(self,heat,fan):
+        """ Add required Formula here """
         return ((self.k*(1-math.exp(-(heat-fan)/self.z)))+self.c)
