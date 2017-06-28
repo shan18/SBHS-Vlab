4b9a62eae9089f20b0d718ac6abfdc56656508b0
codegeek96 <thegeek.004@gmail.com>
2017-06-28 15:16:45 +0530

    changes in the formula

--- sbhs_formula/formula.py
+++ sbhs_formula/formula.py
@@ -4,7 +4,7 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-x = os.path.join(BASE_DIR, 'coeff_2nd_order.txt')
+x = os.path.join(BASE_DIR, 'coeff.txt')
 
 
 class Formula:
@@ -13,27 +13,17 @@ class Formula:
         self.coeff = []
         with open(x, 'r') as f:
             lines = f.readlines()
+        # coeff.txt has values in format: mass  resistance  specific heat
         self.coeff = [float(c) for c in lines[i].split()]
 
     def getTemp(self, heat, fan, instantaneous_time):
         """ Add required Formula here """
-        k = self.coeff[0]
-
-        """ 1st Order Model
-        tow = self.coeff[1]
-        exp_power = -1 * (instantaneous_time / tow)
-        value = k * (1 - math.exp(exp_power))
-        heat_value = (value * heat) + random.uniform(-0.5, 0.5)
-        """
-
-        # 2nd Order Model
-        tow_1 = self.coeff[1]
-        tow_2 = self.coeff[2]
-        exp_power_1 = -1 * (instantaneous_time / tow_1)
-        exp_power_2 = -1 * (instantaneous_time / tow_2)
-        val = (tow_1 * math.exp(exp_power_1) - tow_2 * math.exp(exp_power_2)) / (tow_1 - tow_2)
-        heat_value = k * heat * (1 - val)
-        return heat_value + random.uniform(-0.5, 0.5)
+        mass = self.coeff[0]
+        resistance = self.coeff[1]
+        sp_heat = self.coeff[2]
+        current = heat / 50
+        heat_in_joules = pow(current, 2) * resistance
+        return heat_in_joules / (mass * sp_heat)
 
     @staticmethod
     def countCoeff():
