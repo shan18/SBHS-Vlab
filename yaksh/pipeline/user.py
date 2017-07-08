432bc1f37eee8feb683a9fe0dacab3e1b1e6fa22
codegeek96 <thegeek.004@gmail.com>
2017-07-08 20:19:24 +0530

    remove the social sign in from yaksh

--- yaksh/pipeline/user.py
+++
@@ -1,8 +0,0 @@
-from yaksh.models import Profile
-#from django.contrib.auth.models import User
-
-def save_profile(backend, user, response, *args, **kwargs):
-    if not hasattr(user, 'profile'):
-        profile = Profile.objects.create(user=user)
-        profile.roll_number = profile.id
-        profile.save()
