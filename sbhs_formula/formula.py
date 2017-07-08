8b3aadd342beb0862e1c99d884ba0d7d45ea2a86
codegeek96 <thegeek.004@gmail.com>
2017-07-08 20:23:41 +0530

    remove unused files

--- sbhs_formula/formula.py
+++ sbhs_formula/formula.py
@@ -4,7 +4,7 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-x = os.path.join(BASE_DIR, 'coeff.txt')
+x = os.path.join(BASE_DIR, 'coeff_1st_order.txt')
 
 
 class Formula:
@@ -13,21 +13,31 @@ class Formula:
         self.coeff = []
         with open(x, 'r') as f:
             lines = f.readlines()
-        # coeff.txt has values in format: mass  resistance  specific heat
         self.coeff = [float(c) for c in lines[i].split()]
 
     def get_temp(self, heat, fan, instantaneous_time):
         """ Add required Formula here """
-        mass = self.coeff[0]
-        resistance = self.coeff[1]
-        sp_heat = self.coeff[2]
-        current = heat / 50
-        heat_in_joules = pow(current, 2) * resistance
-        return heat_in_joules / (mass * sp_heat)
+        k = self.coeff[0]
+
+        # 1st Order Model
+        tow = self.coeff[1]
+        exp_power = -1 * (instantaneous_time / tow)
+        value = k * (1 - math.exp(exp_power))
+        return value * heat + random.uniform(-0.5, 0.5)
+
+        """ 2nd Order Model
+        tow_1 = self.coeff[1]
+        tow_2 = self.coeff[2]
+        exp_power_1 = -1 * (instantaneous_time / tow_1)
+        exp_power_2 = -1 * (instantaneous_time / tow_2)
+        val = (tow_1 * math.exp(exp_power_1) - tow_2 * math.exp(exp_power_2)) / (tow_1 - tow_2)
+        heat_value = k * heat * (1 - val)
+        return heat_value + random.uniform(-0.5, 0.5)
+        """
 
     @staticmethod
     def count_coeff():
         """ This method counts the number of Coefficient Sets available"""
-        with open(x, 'r') as f:
+        with open(x,'r') as f:
             lines = f.readlines()
         return len(lines)
